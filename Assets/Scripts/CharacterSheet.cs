4888010c694e42b8c3736226df6d3d304f8ba553
Alastair <alas_lair@proton.me>
2025-01-24 12:57:16 -0600

    Add stats

--- Assets/Scripts/CharacterSheet.cs
+++ Assets/Scripts/CharacterSheet.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CharacterSheet : MonoBehaviour
 {
+    [SerializeField] string characterName;
+    [SerializeField] int proficiency;
+    [SerializeField] bool finesseWeapon;
+    [SerializeField][Range(-5, 5)] int strength;
+    [SerializeField][Range(-5, 5)] int dexterity;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
